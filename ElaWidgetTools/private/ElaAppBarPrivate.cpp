e92497b72440f8981ba5670a822074ecc94fd4f7
Ela <liniyous@gmail.com>
2025-07-30 11:26:58 +0800

    ElaAppBar最大化按钮功能优化 支持顶部拖动

--- ElaWidgetTools/private/ElaAppBarPrivate.cpp
+++ ElaWidgetTools/private/ElaAppBarPrivate.cpp
@@ -189,6 +189,10 @@ bool ElaAppBarPrivate::_containsCursorToItem(QWidget* item)
             return false;
         }
     }
+    else if (item == _maxButton)
+    {
+        rect.adjust(0, 8, 0, 0);
+    }
     if (rect.contains(point))
     {
         return true;
@@ -248,7 +252,7 @@ int ElaAppBarPrivate::_calculateMinimumWidth()
         }
     }
     QList<QAbstractButton*> buttonList = q->findChildren<QAbstractButton*>();
-    for (auto button : buttonList)
+    for (auto button: buttonList)
     {
         if (button->isVisible() && button->objectName() != "NavigationButton")
         {
